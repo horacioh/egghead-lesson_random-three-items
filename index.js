e6e4212762452805edad5f8c484b2a20a0063b01
Horacio Herrera <hi@horacioh.com>
2020-08-15 04:04:05 +0200

    final code

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ const items = [
   "Nader", "John", "Rich", "Sean", "Dan", "Kyle"
 ];
 
+function getRandom3(items) {
+  return [...items]
+    .sort(() => Math.random() > 0.5 ? 1 : -1)
+    .slice(0, 3)
+}
 
+console.log(getRandom3(items))
